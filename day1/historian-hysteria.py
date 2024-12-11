e89856aeb85accf7e430bf1ec8383dd4708921e8
Yadav106 <sahil.jp.yadav@gmail.com>
2024-12-11 14:15:03 +0530

    done part one of day 1

--- day1/historian-hysteria.py
+++ day1/historian-hysteria.py
@@ -1,1 +1,24 @@
-print("Welcome to AOC-2023")
+with open("input.txt", "r") as f:
+    ip_text = f.read()
+
+ip_lines = ip_text.splitlines()
+
+arr_a = []
+arr_b = []
+
+for line in ip_lines:
+    temp = line.split()
+    arr_a.append(int(temp[0].strip()))
+    arr_b.append(int(temp[1].strip()))
+
+arr_a = sorted(arr_a)
+arr_b = sorted(arr_b)
+
+tot_distance = 0
+
+
+for i in range(len(arr_a)):
+    tot_distance += abs(arr_a[i] - arr_b[i])
+
+
+print(tot_distance)
